7acf02f952b3e21556a65d44f61fc07d5a3c926d
talyssonoc <talyssonoc@gmail.com>
2015-04-08 19:50:38 -0300

    Add new react_ujs

--- assets/react_ujs.js
+++ assets/react_ujs.js
@@ -1,8 +1,44 @@
-$(function () {
-  $('[data-react-class]').each(function() {
-    var reactClass = React.createFactory(window[$(this).attr('data-react-class')]);
-    var props = JSON.parse($(this).attr('data-react-props'));
+;(function(document, window) {
 
-    React.render(reactClass(props), this);
-  });  
-});
+  window.ReactLaravelUJS = {
+    CLASS_NAME_ATTR: 'data-react-class',
+    PROPS_ATTR: 'data-react-props',
+
+    getElements: function getElements() {
+      var finder = jQuery || document.querySelectorAll;
+
+      return finder('[' + ReactLaravelUJS.CLASS_NAME_ATTR + ']');
+    },
+
+    mountComponents: function mountComponents() {
+      var elements = ReactLaravelUJS.getElements();
+      var element;
+      var reactClass;
+      var props;
+
+      for(var i = 0; i < elements.length; i++) {
+        element = elements[i];
+        reactClass = window[element.getAttribute(ReactLaravelUJS.CLASS_NAME_ATTR)];
+        props = JSON.parse(element.getAttribute(ReactLaravelUJS.PROPS_ATTR));
+
+        React.render(React.createElement(reactClass, props), element);
+      }
+    },
+
+    unmountComponents: function unmountComponents() {
+      var elements = ReactLaravelUJS.getElements();
+
+      for(var i = 0; i < elements.length; i++) {
+        React.unmountComponentAtNode(elements[i]);
+      }
+    },
+
+    handleEvents: function handleEvents() {
+      document.addEventListener('DOMContentLoaded', ReactLaravelUJS.mountComponents);
+      window.addEventListener('unload', ReactLaravelUJS.unmountComponents);
+    }
+  };
+
+  ReactLaravelUJS.handleEvents();
+
+})(document, window);
