911cdae2806b6ffbeb01133cb1562d7e7d037d72
Will Vincent <will.vincent@ffwagency.com>
2016-10-26 10:40:05 -0500

    Add comments to config, change components to an array

--- config/config.php
+++ config/config.php
@@ -1,8 +1,50 @@
 <?php
 
 return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | React Source
+  |--------------------------------------------------------------------------
+  |
+  | Path to the react.js file.
+  |
+  */
+
   'source'     => public_path('vendor/react-laravel/react.js'),
+
+  /*
+  |--------------------------------------------------------------------------
+  | React-Dom Source
+  |--------------------------------------------------------------------------
+  |
+  | Path to the react-dom.js file.
+  |
+  */
+
   'dom-source' => public_path('vendor/react-laravel/react-dom.js'),
+
+  /*
+  |--------------------------------------------------------------------------
+  | React-Dom-Server Source
+  |--------------------------------------------------------------------------
+  |
+  | Path to the react-dom-server.js file.
+  |
+  */
+
   'dom-server-source' => public_path('vendor/react-laravel/react-dom-server.js'),
-  'components' => public_path('js/components.js')
+
+  /*
+  |--------------------------------------------------------------------------
+  | Components
+  |--------------------------------------------------------------------------
+  |
+  | An array of paths to the javascript files containing your components.
+  |
+  */
+
+  'components' => [
+    public_path('js/components.js')
+  ]
 ];
